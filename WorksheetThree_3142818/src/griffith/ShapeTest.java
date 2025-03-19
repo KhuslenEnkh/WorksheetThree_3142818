b136309637a779f2820a8ddc6cffb4b8ade19611
Khuslen <khuslen.enkhamgalan@student.griffith.ie>
2025-03-19 01:04:40 +0000

    Add ShapeTest class with initial test cases for Shape functionality

---
+++ WorksheetThree_3142818/src/griffith/ShapeTest.java
@@ -0,0 +1,40 @@
+//Khuslen Enkh-Amgalan, ID:3142818, 19/03/2025
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShapeTest {
+
+	@Test
+	void testShape() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPerimeter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
