692b29b3a42ce0e8b1e99da7a350f76639071707
Khuslen <khuslen.enkhamgalan@student.griffith.ie>
2025-03-19 00:36:49 +0000

    Add name property and methods to abstract Shape class

--- WorksheetThree_3142818/src/griffith/Shape.java
+++ WorksheetThree_3142818/src/griffith/Shape.java
@@ -1,5 +1,29 @@
 package griffith;
 
 public abstract class Shape {
-	
+	private String name;
+
+    //Constructor
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    //Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Abstract methods
+    public abstract double area();
+    public abstract double perimeter();
+
+    //toString method
+    @Override
+    public String toString() {
+        return "Shape: " + name;
+    }
 }
