17cc1270406fb7608c82cbbfab8931858e34e87e
Khuslen <khuslen.enkhamgalan@student.griffith.ie>
2025-03-19 01:38:19 +0000

    Implement area and perimeter methods in Rhombus class

--- WorksheetThree_3142818/src/griffith/Rhombus.java
+++ WorksheetThree_3142818/src/griffith/Rhombus.java
@@ -42,12 +42,12 @@ public class Rhombus extends Shape {
 
     @Override
     public double area() {
-        return 0; 
+    	return (diagonal1 * diagonal2) / 2;
     }
 
     @Override
     public double perimeter() {
-        return 0; 
+    	return 4 * side;
     }
 
     // Override toString
